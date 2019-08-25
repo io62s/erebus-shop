f0b6f8a4f92077f9d3bfd821e51610d357ec59b5
io62s <ufo62s@gmail.com>
2019-08-25 23:21:11 +0200

    homePage styles updated responsive, redux added

--- src/components/menu-item/MenuItem.js
+++ src/components/menu-item/MenuItem.js
@@ -6,9 +6,12 @@ function MenuItem({ title, imageUrl, size, linkUrl, history, match }) {
   return (
     <div
       className={`menu-item ${size}`}
-      style={{ backgroundImage: `url(${imageUrl})` }}
       onClick={() => history.push(`${linkUrl}`)}
     >
+      <div
+        className="image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      ></div>
       <div className="content">
         <h1 className="title">{title}</h1>
         <span className="subtitle">Shop Now</span>
