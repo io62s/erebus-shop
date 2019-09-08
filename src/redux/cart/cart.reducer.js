c95fc195104e462926f9b0cf44fb1a6030f7fa6c
io62s <ufo62s@gmail.com>
2019-09-08 23:28:58 +0200

    add remove arrows to checkout item added, add remove func added

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,12 +1,17 @@
 import CartActionTypes from "./cart.types";
-import { addItemToCart } from "./cart.utils";
+import { addItemToCart, removeItem } from "./cart.utils";
 
 const INITIAL_STATE = {
   hidden: true,
   cartItems: []
 };
 
-const { TOGGLE_CART_HIDDEN, ADD_ITEM } = CartActionTypes;
+const {
+  TOGGLE_CART_HIDDEN,
+  ADD_ITEM,
+  CLEAR_ITEM_FROM_CART,
+  REMOVE_ITEM
+} = CartActionTypes;
 
 const cartReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
@@ -20,6 +25,19 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload)
       };
+
+    case REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItem(state.cartItems, action.payload)
+      };
+
+    case CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(item => item.id !== action.payload.id)
+      };
+
     default:
       return state;
   }
